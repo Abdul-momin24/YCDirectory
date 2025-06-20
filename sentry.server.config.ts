c2948e8a8f4fdc13e58215b574879cf8de20101a
Abdul-momin24 <khanabdulmomin24@gmail.com>
2025-06-20 15:39:52 +0530

    Added sentry for live fixing of bugs caused by user

---
+++ sentry.server.config.ts
@@ -0,0 +1,29 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://9d999d0035b86e712089f5e40131cfbe@o4509529495568384.ingest.us.sentry.io/4509529514639360",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
+}
+
+export const onRequestError = Sentry.captureRequestError;
